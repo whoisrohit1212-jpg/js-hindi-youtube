838af9c008427d96e29db1ce7b9c712332ec8ab3
whoisrohit1212-jpg <whoisrohit1212@gmail.com>
2026-08-18 09:52:30 +0000

    datatype summarry

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+const score = 100
+const scoreValue = 100.6
+//console.log(typeof score);
+
+// const isLoggedIn = false
+// console.log(typeof isLoggedIn);
+const outsideTemp = null
+//console.log(typeof outsideTemp);
+
+const id = Symbol('2323')
+//console.log(typeof id);
+const secondId = Symbol('2323')
+//console.log(id === secondId);
+
+const bigNumber = 2323423432333233343323n
+//console.log(typeof bigNumber);
+
+const smarthphone = ["oneplus","realme","samsung"]
+//console.log(typeof smarthphone);
+
+let myObj = {
+    name: "harsh",
+    age: 24,
+}
+console.log(typeof myObj);
+
+
+
+
